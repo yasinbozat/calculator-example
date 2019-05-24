14d634e7a69a36877a85fd1ee222dbb07f44f859
Yasin Bozat <yasinbozatr@gmail.com>
2019-05-24 16:49:18 +0300

    Update
    Added print to screen and select

--- Calculator.cpp
+++ Calculator.cpp
@@ -1,6 +1,7 @@
 //Calcultor
 #include<stdio.h>
-
+#include<stdlib.h>
+#include<windows.h>
 
 int division(int value1,int value2){
 	int sum = 0;
@@ -28,6 +29,41 @@ int addition(int value1,int value2){
 
 
 main(){
+	base:
+	int select = 0,value1,value2;
+	printf("----------------------------------------------------------------------\n");
+	printf("1 - Addition \n2 - Subtraction \n3 - Multiplication \n4 - Division \n\nSelect =   ");
+	scanf("%d",&select);
+	if(select < 1 || select > 4){
+		system("cls");
+		printf("Wrong Select! Please Try Again.");
+		Sleep(1500);
+		system("cls");
+		goto base;
+	}
+	printf("----------------------------------------------------------------------\n");
+	printf("\nEnter Value 1 = ");	scanf("%d",&value1);
+	printf("Enter Value 2 = ");	scanf("%d",&value2);
+	printf("\n----------------------------------------------------------------------\n");
 	
+	switch (select){
+	case 1: printf("\nSelected Addition");
+	printf("\nResult = %d",addition(value1,value2));
+	
+	break;
+	
+	case 2: printf("\nSelected Subtraction");
+	printf("\nResult = %d",subtraction(value1,value2));
+	break;
+	
+	case 3: printf("\nSelected Multiplication");
+	printf("\nResult = %d",multiplication(value1,value2));
+	break;
+	
+	case 4: printf("\nSelected Division");
+	printf("\nResult = %d",division(value1,value2));
+	break;
+	
+	}
 }
 
